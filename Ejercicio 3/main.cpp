a73035f67a91f8cd5cd1a71659c8d5e67b8d8236
NlodeL <nlodelop@myuax.com>
2023-11-16 13:18:22 +0100

    añado las pausas

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -7,10 +7,16 @@ int main() {
     cout << "Ingrese el radio del circulo para calcular el area: ";
     calcularAreaCirculo(cin, cout);
     cout << endl;
+    cout << "Pulse enter para continuar... \n";
+    cin.ignore();
+    cin.get();
 
     cout << "Ingrese el radio del circulo para calcular el perimetro: ";
     calcularPerimetroCirculo(cin, cout);
     cout << endl;
+    cout << "Pulse enter para continuar... \n";
+    cin.ignore();
+    cin.get();
 
     return 0;
 }
